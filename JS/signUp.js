92f7a1fcb1e1876681a569de7b6476ee0fc8c5c0
Lena-user <le.lena5415@gmail.com>
2024-12-22 05:58:11 +0700

    User separate into First Name and Last Name, Format File is <FistName>_<LastName>.<FileType>

--- JS/signUp.js
+++ JS/signUp.js
@@ -2,13 +2,14 @@ const sign_up = document.getElementById("signUp");
 sign_up.addEventListener('submit', function (e) {
     e.preventDefault()
 
-    const user = document.getElementById("User").value;
+    const firstName = document.getElementById("FirstName").value;
+    const lastName = document.getElementById("LastName").value
     const image = document.getElementById("imageInput")
     const file = image.files[0]
     const fileType = file.type.split('/')[1];
 
     const bucket_name = 'registrate-storage'
-    const api_url = `https://xl0lcefokd.execute-api.ap-southeast-2.amazonaws.com/my_stage/${bucket_name}/${user}.${fileType}`;
+    const api_url = `https://xl0lcefokd.execute-api.ap-southeast-2.amazonaws.com/my_stage/${bucket_name}/${firstName}_${lastName}.${fileType}`;
 
     fetch(api_url, {
         method: 'PUT',
